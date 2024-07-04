487f4279cdc4dc9b01835032b24844554124beb4
Putra Prasetya <putraprasetya333@gmail.com>
2024-07-04 21:24:29 +0700

    Setting json-server & Get Products API

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,40 @@
-import Card2 from '../../components/module/Card2'
+import { useEffect, useState } from 'react'
+import Card from '../../components/Card'
+import api from '../../config/api'
 
 const Home = () => {
+  const [products, setProducts] = useState([])
+
+  const getProducts = async () => {
+    try {
+      const response = await api.get('/product')
+      setProducts(response.data)
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(()=> {
+    getProducts()
+  },[])
+
   return (
-    <div className="w-full h-screen bg-[url('../img/home/bg.webp')] bg-cover bg-center flex flex-col items-center">
+    <main className="w-full h-screen bg-[url('../img/home/bg.webp')] bg-cover bg-center flex flex-col items-center">
       <h1 className="text-white text-4xl mt-8">Get Snacks Now !!</h1>
       <h3 className="text-gray-400 mt-2">The most delicious snacks are available here. Transactions are fast, safe and reliable.Get it soon !</h3>
-      <Card2 />
-    </div>
+      <div className='w-full flex flex-wrap justify-evenly gap-y-12 mt-8'>
+        {
+          products?.map((item, index) => (
+            
+            <Card data={item} key={index} />
+            // <div key={index}>
+            //   <p className='text-white'>{item.title}</p>
+            // </div>
+          ))
+        }
+      </div>
+      
+    </main>
   )
 }
 
--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -1,4 +1,24 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import api from '../../config/api'
+
 const Order = () => {
+  const { id } = useParams()
+  const [product, setProduct] = useState({})  
+
+  const getProduct = async () => {
+    try {
+      const response = await api.get('/product')
+      setProduct(response.data)
+    } catch(e) {
+      console.log(e)
+    }
+  }
+
+  useEffect(() => {
+    getProduct()
+  })
+
   return (
     <div>Order</div>
   )
