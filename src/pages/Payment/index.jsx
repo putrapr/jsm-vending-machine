8c04a55c19213c9769da27dc2815f96d1825bca4
Putra Prasetya <putraprasetya333@gmail.com>
2024-07-05 11:23:23 +0700

    Feat: Loading Put Money

--- src/pages/Payment/index.jsx
+++ src/pages/Payment/index.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
+import { Link, useParams, useNavigate } from 'react-router-dom'
 import api from '../../config/api'
 import Swal from 'sweetalert2'
 
@@ -37,21 +37,44 @@ const Payment = () => {
         icon: 'error',
         text: "Sorry, you don't have enough money",
       })
-    else {
+    else {      
+      Swal.fire({
+        title: 'Put money into the machine',
+        html: 'Please wait while we process your request.',
+        allowOutsideClick: false,
+        didOpen: () => {
+          Swal.showLoading()
+        }
+      })
       const response = await api.get('/product/'+id)
       const data = { stock: response.data.stock - 1 }           
-      api.patch('/product/'+id, data)
+      api.patch('/product/'+id, data)   
 
       const change = totalMoney - product.price
-      Swal.fire({
-        icon: 'success',
-        title: 'Payment Successful',
-        text: 'Your money back: Rp.'+ change
-      })
-      navigate('/')
+      setTimeout(() => {
+        Swal.close()
+        if (change == 0)
+          Swal.fire({
+            icon: 'success',
+            title: 'Payment Successful'
+          }).then((result) => {
+            if (result.isConfirmed)
+              navigate('/')     
+          })
+
+        else
+          Swal.fire({
+            icon: 'success',
+            title: 'Payment Successful',
+            text: 'Your money back: Rp.'+ change
+          }).then((result) => {
+            if (result.isConfirmed)
+              navigate('/')     
+          })
+      }, 5000)
     }
   }
-
+  
   useEffect(() => {
     getProduct()
     getTotal()
@@ -67,7 +90,8 @@ const Payment = () => {
         <div className='w-96 flex flex-col items-center border p-4 bg-gray-800 bg-opacity-90'>
           <img src={product?.img_url} alt="order snack" className='w-72' />
           <h1 className='font-semibold text-3xl text-white mt-4 text-center'>{product?.title}</h1>
-          <h3 className='mt-4 text-gray-100'>Price: Rp.{product?.price}</h3>          
+          <h3 className='mt-4 text-gray-100'>Price: Rp.{product?.price}</h3>
+          <Link to="/" type="button" className="text-white bg-gray-800 hover:bg-gray-900 active:outline-none active:ring-4 active:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 mt-8">Back</Link>         
         </div>
       </div>
       <div className='w-96 border p-6 bg-gray-800 bg-opacity-90'>
